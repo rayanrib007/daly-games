5a86f353531f343a57bffe72007e5445b7c4db05
Rayan Ribeiro Silva <rayan.silva@artvac.local>
2024-12-10 08:36:53 -0300

    Added home settings and main header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import CHeaderGlobal from "@/components/header/CHeaderGlobal";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,10 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CHeaderGlobal />
         {children}
       </body>
     </html>
---
+++ src/components/header/CHeaderGlobal.tsx
@@ -0,0 +1,7 @@
+export default function CHeaderGlobal() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <h1>CHeaderGlobal</h1>
+    </header>
+  );
+}
