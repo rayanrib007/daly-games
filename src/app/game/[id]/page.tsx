13f7ad173c661ca14457e06527c8ffc3b3189a66
Rayan Ribeiro <rayanribeiro99@gmail.com>
2024-12-27 08:01:40 -0300

    Added functionality to generate personalized metadata according to each game

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,5 +1,48 @@
 import CTNGameDetails from "@/containers/game/CTNGameDetails";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const id = (await params).id;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`,
+      { cache: "no-store" }
+    )
+      .then((res) => res.json())
+      .catch(() => {
+        return {
+          title: "DalyGames - Descubra jogos incríveis para se divertir",
+        };
+      });
+    return {
+      title: response.title,
+      description: `${response.description.slice(0, 150)}...`,
+      openGraph: {
+        title: response.title,
+        images: [response.image_url],
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
+        },
+      },
+    };
+  } catch (error) {
+    return {
+      title: "DalyGames - Descubra jogos incríveis para se divertir",
+    };
+  }
+}
 
 async function getGameData(id: string) {
   try {
